86754f0fdde2d8a89da959cc815d28bec13e0556
shama <assiddik001@gmail.com>
2023-07-28 09:08:49 +0600

    service done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import RootLayout from "@/components/Layouts/RootLayout";
 import Banner from "@/components/UI/Banner";
+import Services from "@/components/UI/Services";
 
 const Home = () => {
   return (
     <main className="layout">
       <Banner />
-      <div className="body_wrapper md:px-[50px] p-[20px]"></div>
+      <div className="body_wrapper md:px-[50px] p-[20px]">
+        <Services />
+      </div>
     </main>
   );
 };
