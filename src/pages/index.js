25bf7bc6c71b5a2860eeb98ef827cb8a1380c34a
shama <assiddik001@gmail.com>
2023-07-28 08:52:41 +0600

    banner part done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
 import RootLayout from "@/components/Layouts/RootLayout";
+import Banner from "@/components/UI/Banner";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main className="layout">
+      <Banner />
+      <div className="body_wrapper md:px-[50px] p-[20px]"></div>
+    </main>
+  );
 };
 
 export default Home;
